b7b45ee5e51d3778fbc92c602da3130d76f81782
F1ZZ <TheKubonn@hotmail.com>
2024-07-22 14:22:41 +0200

    Finish first project from Unity Pathways

--- Unity Pathways - Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unity Pathways - Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    private float speed = 15f;
+    private float turnSpeed = 40f;
+    private float horizontalInput;
+    private float forwardInput;
     
     void Update()
     {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * 20));
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
